6beccca3e605835c2ed5a584191bdcc388c3211d
Jérôme Leclercq <lynix680@gmail.com>
2021-05-11 11:19:40 +0200

    Ajout d'exemple de classe

---
+++ SDL/src/SDLInitializer.cpp
@@ -0,0 +1,16 @@
+#include "SDLInitializer.hpp"
+
+SDLInitializer::SDLInitializer()
+{
+	// Implémentation du constructeur
+}
+
+SDLInitializer::~SDLInitializer()
+{
+	// Implémentation du destructeur
+}
+
+void SDLInitializer::Methode()
+{
+	// Implémentation de la méthode
+}
---
+++ SDL/src/SDLInitializer.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+class SDLInitializer
+{
+public:
+	SDLInitializer();
+	~SDLInitializer();
+
+	void Methode();
+};
